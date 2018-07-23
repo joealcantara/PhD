50e3a49da2f75835148e246cf01fa6695c5927cb
Jomar Alcantara <joe@joealcantara.com>
2018-07-23 09:19:27 +0100

    added PCC work

---
+++ Python/PythonCrashCourse/personal_message.py
@@ -0,0 +1,2 @@
+name = "Jomar Alcantara"
+print("Hello " + name + ", would you like to learn some Python today?")
---
+++ Python/PythonCrashCourse/stripping_names.py
@@ -0,0 +1,4 @@
+name = '\tJomar \t\nAlcantara      \t'
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
